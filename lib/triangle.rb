dac7d97699254f1bab9310b87bed08066628165f
Beth Ruff <beth.ruff2@gmail.com>
2020-02-25 04:25:04 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,8 +7,7 @@ class Triangle
   end
   
   def kind 
-    if (@side1 + @side2 != @side3) || (@side1 <= 0) || (@side2 <= 0) || (@side3 <= 0)
-      begin 
+    if (@side1 <= 0) || (@side2 <= 0) || (@side3 <= 0)
         raise TriangleError 
           puts error.message
       end
