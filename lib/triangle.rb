1565490a7ae3d9cb5c1b2f27616b7be5d24f3972
Beth Ruff <beth.ruff2@gmail.com>
2020-02-25 04:17:49 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,8 +7,9 @@ class Triangle
   end
   
   def kind 
-    if (@side1 == @side2) && (@side2 == @side3) && (@side1 == @side3)
-      
+    if (@side1 == @side2) && (@side2 == @side3)
+      :equilateral
+    elsif 
   end 
   
 end
